6031846d9b26acfe42d152aace9cd708305ca97f
Shomari Roberts <shomariroberts@gmail.com>
2020-10-12 00:22:31 -0400

    .

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,24 +5,23 @@ const db = require('../database/dbConfig');
 
 describe('server', () => {
   describe('POST /api/auth/login', () => {
-    it('should return 200 on success', () => {
-      return request(server)
-        .post('/login')
-        .send({ username: 'user1', password: 'password' })
-        .then((res) => {
-          expect(res.status).toBe(200);
-        });
+    it('should return 200 on success', async () => {
+      const res = await request(server)
+        .post('/api/auth/login')
+        .send({ username: 'user1', password: 'password' });
+
+      expect(res.status).toBe(200);
     });
-    it('should return a message with a welcome message and an auth token', () => {
-      return request(server)
+
+    it('should return a message with a welcome message and an auth token', async () => {
+      const res = await request(server)
         .post('/login')
-        .send({ username: 'user1', password: 'password' })
-        .then((res) => {
-          expect(res.body).toMatchObject({
-            message: 'Welcome to the api',
-            token: expect.any(String),
-          });
-        });
+        .send({ username: 'user1', password: 'password' });
+
+      expect(res.body).toMatchObject({
+        message: 'Welcome to the api',
+        token: expect.any(String),
+      });
     });
   });
 
@@ -31,43 +30,37 @@ describe('server', () => {
       await db('users').truncate();
     });
 
-    it('should return 201 on success', () => {
-      return request(server)
+    it('should return 201 on success', async () => {
+      const res = await request(server)
         .post('/register')
-        .send({ username: 'user1', password: 'password' })
-        .then((res) => {
-          expect(res.status).toBe(201);
-        });
+        .send({ username: 'user1', password: 'password' });
+
+      expect(res.status).toBe(201);
     });
-    it('should return a message with an id, a hashed password, and a username', () => {
-      return request(server)
+
+    it('should return a message with an id, a hashed password, and a username', async () => {
+      const res = await request(server)
         .post('/register')
-        .send({ username: 'user1', password: 'password' })
-        .then((res) => {
-          expect(res.body[0]).toMatchObject({
-            id: expect.any(Number),
-            password: expect.not.stringMatching('password'),
-            username: 'user1',
-          });
-        });
+        .send({ username: 'user1', password: 'password' });
+
+      expect(res.body[0]).toMatchObject({
+        id: expect.any(Number),
+        password: expect.not.stringMatching('password'),
+        username: 'user1',
+      });
     });
   });
   describe('GET to /api/jokes', () => {
-    it('should return 500 status if user is unauthorized', () => {
-      return request(server)
-        .get('/api/jokes')
-        .then((res) => {
-          expect(res.status).toBe(500);
-        });
+    it('should return 400 status if user is unauthorized', async () => {
+      const res = await request(server).get('/api/jokes');
+
+      expect(res.status).toBe(400);
     });
-    it("should return an error message indicating why the user can't access it", () => {
-      return request(server)
-        .get('/api/jokes')
-        .then((res) => {
-          expect(res.body).toMatchObject({
-            message: 'Error Fetching Jokes',
-          });
-        });
+
+    it('should return json', async () => {
+      const res = await server.get('/api/jokes');
+
+      expect(res.type).toBe('application/json');
     });
   });
 });
